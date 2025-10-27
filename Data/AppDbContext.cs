bf3e77f152d2f2d925581bf119295d23e17ace1b
Daniil Ivashshev <ivasevdaniil@gmail.com>
2025-10-27 11:36:17 +0200

    feat: add model (Dog) and Db Context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DogHouseService.Models;
+
+namespace DogHouseService.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Dog> Dogs { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Dog>(eb =>
+        {
+            eb.HasKey(d => d.Id);
+            eb.HasIndex(d => d.Name).IsUnique();
+            eb.Property(d => d.Name).IsRequired();
+            eb.Property(d => d.Color).IsRequired();
+        });
+    }
+}
